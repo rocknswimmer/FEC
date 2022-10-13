b0b4253b78bff125d3567f93023e548947fd2c6c
Gabe Yamartino <gabeyamartino@gmail.com>
2022-10-13 15:47:41 -0400

    installed express and axios, created Review component

---
+++ client/src/components/Reviews/Review.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+const Reviews = (props) => {
+
+  return (
+    <div>
+      <div>I'm a Reviews component</div>
+    </div>
+  );
+};
+
+export default Reviews;
