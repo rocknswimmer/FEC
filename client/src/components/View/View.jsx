abcec81bd486511a35e03d838773e70beb2916e3
Corinna Cole <corinna.j.cole@gmail.com>
2022-10-14 12:31:05 -0400

    create basic subcomponents within View

--- client/src/components/View/View.jsx
+++ client/src/components/View/View.jsx
@@ -1,11 +1,21 @@
 import React, { useState, useEffect } from 'react';
+import Description from './Description.jsx';
+import ProductImage from './ProductImage.jsx';
+import ProductName from './ProductName.jsx';
+import SelectedStyle from './SelectedStyle.jsx';
+import ItemSelection from './SelectedStyle.jsx';
 
 const View = ({productId}) => {
   return (
     <div>
       I'm the View.jsx and this is the product Id {productId} i received from App
+      <ProductImage productId = {productId}/>
+      <ProductName productId = {productId}/>
+      <SelectedStyle productId = {productId}/>
+      <ItemSelection productId = {productId}/>
+      <Description productId = {productId}/>
     </div>
-  )
+  );
 };
 
 export default View;
