127bd469b86af59e4e8c76a67b3b4a36452e2566
Corinna Cole <corinna.j.cole@gmail.com>
2022-10-14 16:20:28 -0400

    begin making get requests and routes

--- client/src/components/View/View.jsx
+++ client/src/components/View/View.jsx
@@ -1,19 +1,59 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Description from './Description.jsx';
 import ProductImage from './ProductImage.jsx';
 import ProductName from './ProductName.jsx';
 import SelectedStyle from './SelectedStyle.jsx';
 import ItemSelection from './SelectedStyle.jsx';
 
-const View = ({productId}) => {
+const View = ({ productId }) => {
+  //Establish pieces of state for View and StyleSelector
+  const [currentProduct, setCurrentProduct] = useState({
+    "id": 37311,
+    "campus": "hr-rfe",
+    "name": "",
+    "slogan": "",
+    "description": "",
+    "category": "",
+    "default_price": "",
+    "created_at": "",
+    "updated_at": "",
+    "features": [
+      {
+        "feature": "",
+        "value": ""
+      }
+    ]
+  });
+
+  const [currentStyle, setCurrentStyle] = useState({});
+
+  //Get methods for View and StyleSelector
+  const getCurrentProduct = (productId) => {
+    axios.get('/products', {
+      params: {
+        "id": productId
+      }
+    })
+      .then(results => {
+        console.log('Here are the successful results of of getCurrentProduct ', results);
+        setCurrentProduct(results);
+      })
+      .catch(err => {
+        console.log('There is an error in the getCurrentProduct fn ', err);
+      })
+  };
+
+  const getCurrentStyle = () => { };
+
   return (
     <div>
       I'm the View.jsx and this is the product Id {productId} i received from App
-      <ProductImage productId = {productId}/>
-      <ProductName productId = {productId}/>
-      <SelectedStyle productId = {productId}/>
-      <ItemSelection productId = {productId}/>
-      <Description productId = {productId}/>
+      <ProductImage />
+      <ProductName />
+      <SelectedStyle />
+      <ItemSelection />
+      <Description />
     </div>
   );
 };
--- server/index.js
+++ server/index.js
@@ -7,4 +7,8 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 app.listen(3001);
 
+app.get('/products', (req, res) => {
+
+});
+
 console.log('Listening on port 3001');
