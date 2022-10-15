d72d45538cf03af57a246fc133dbe16772f502c2
Corinna Cole <corinna.j.cole@gmail.com>
2022-10-14 21:28:37 -0400

    refactor server and begin drafting test

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.get('/products', (req, res) => {
 
   axios.get(urlVariable, options)
     .then(results => {
-      console.log('these are the /products response from the api -->', results);
+      // console.log('these are the /products response from the api -->', results);
       res.send(results.data);
     })
     .catch(err => {
@@ -26,6 +26,26 @@ app.get('/products', (req, res) => {
       res.status(404).end();
     });
 
+});
+
+app.get('/products/:product_id/styles', (req, res) => {
+  let urlVariable= `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/${req.query.id}/styles`;
+
+  let options = {
+    "headers": {
+      "Authorization": api
+    }
+  };
+
+  axios.get(urlVariable, options)
+    .then(results => {
+      // console.log('these are the /products/:product_id/styles response from the api -->', results);
+      res.send(results.data);
+    })
+    .catch( err => {
+      console.log('there was an error in the api call for the get product/product_id/styles --> ', err);
+      res.status(404).end();
+    });
 });
 
 //REVIEWS ROUTES
