4dd28e1a8fdde1135479097dc9dc672a43d0d796
Gabe Yamartino <gabeyamartino@gmail.com>
2022-10-17 16:51:54 -0400

    Figuring out how to implement the recommended div in tile

--- client/src/components/Reviews/ReviewsListEntry.jsx
+++ client/src/components/Reviews/ReviewsListEntry.jsx
@@ -68,13 +68,14 @@ const ReviewsListEntry = ({review}) => {
             </div>
           </div> : review.body}
       </div>
+      {/* <div>PUT RECOMMENDED HERE</div> */}
       <div>
         {review.photos.length > 0 ?
           <div>
             {review.photos.map((photo, i) => (
               <div>
-              <Thumbnail src={photo.url} onClick={() => { togglePhoto(photo.url); }} key={i}></Thumbnail>
-              {showModal ? <PhotosModal toggle={togglePhoto} visible={showModal} photo={photo.url}/> : null}
+                <Thumbnail src={photo.url} onClick={() => { togglePhoto(); }} key={i}></Thumbnail>
+                {showModal ? <PhotosModal toggle={togglePhoto} visible={showModal} photo={photo.url}/> : null}
               </div>
             ))}
           </div>
