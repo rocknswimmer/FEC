eb499e78194f2c83ecd5c3e02f08b7038c39226a
Andy Simmons <rocknswimmer116@gmail.com>
2022-10-17 15:00:14 -0400

    created a date formating function that can be imported where needed

--- client/src/components/Questions/Answers.jsx
+++ client/src/components/Questions/Answers.jsx
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import formatedDate from '../../HelperFunctions/formatedDate.js';
 
 
 const Answers = (props) => {
 
-  const helpCount = 'count';
-
   return (
     <div>
       <div>A: {props.answer.body}</div>
-      <span>by {props.answer.answerer_name}, {props.answer.date}</span>
+      <span>by {props.answer.answerer_name}, {formatedDate(props.answer.date)}</span>
       <span> | </span>
       <span>Helpful? <a>Yes</a> {`(${props.answer.helpfulness})`} </span>
       <span> | </span>
