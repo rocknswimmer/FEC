c93c1451c6b30121303230d302949c8112a9a631
Gabe Yamartino <gabeyamartino@gmail.com>
2022-10-17 13:44:19 -0400

    Implemented functionality to truncate both review body, and review title to only allow for the max character length. Implemented button to show the remainder of the review body

--- client/src/components/Reviews/ReviewsListEntry.jsx
+++ client/src/components/Reviews/ReviewsListEntry.jsx
@@ -1,7 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
+const ReviewEntry = styled.div`
+border-bottom: solid grey 1px;
+padding: 30px;
+display: flex-column;
+align-items: space-between;
+font-family: arial;
+`;
+
+const ReviewTitle = styled.div`
+  font-weight: bold;
+`;
+
 const ReviewsListEntry = ({review}) => {
+
+  const [over250, setOver250] = useState(review.body.length > 50);
+
+
+  const toggleShowMore = () => {
+    setOver250(!over250);
+  };
 
   let starTest = (int) => {
     let rating = '';
@@ -11,16 +30,26 @@ const ReviewsListEntry = ({review}) => {
     return rating;
   };
 
-
   return (
-    <div className="rev-entry">
+    <ReviewEntry>
       <div className="rev-star-date">
         <div>{starTest(review.rating)}</div>
         <small><div>{review.reviewer_name}, {review.date}</div></small>
       </div>
-      <b><div>{review.summary}</div></b>
-      <div>{review.body}</div>
-    </div>
+      {review.summary.length > 60 ?
+        <>
+          <ReviewTitle>{review.summary.substring(0, 60)}...</ReviewTitle>
+          <div>...{review.summary.substring(60, review.summary.length)}</div>
+        </> : <ReviewTitle>{review.summary}</ReviewTitle>}
+      <div>
+        {over250 ?
+          <div>
+            {review.body.substring(0, 50)}... <div>
+              <button onClick={() => (toggleShowMore())}>Show More</button>
+            </div>
+          </div> : review.body}
+      </div>
+    </ReviewEntry>
   );
 };
 
