fa2c8a296feb347e2fca82c67d5dc7adc9bb426f
Gabe Yamartino <gabeyamartino@gmail.com>
2022-10-17 12:07:05 -0400

    Add styled components to dependencies, refactor some exisitng css into styled components

--- client/src/components/Reviews/Buttons.jsx
+++ client/src/components/Reviews/Buttons.jsx
@@ -1,10 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import ReviewsModal from './ReviewsModal.jsx';
 
 const Buttons = (props) => {
 
+  const [isModalOpen, setModalIsOpen] = useState(false);
+
   return (
     <div>
-      <div>More Views and Add Reviews Buttons</div>
+      <ReviewsModal />
+      <button className="rev-button">MORE REVIEWS</button>
+      <button className="rev-button">ADD A REVIEW +</button>
     </div>
   );
 };
