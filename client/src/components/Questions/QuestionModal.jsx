eeb5efcd5a515eb567b0af01b80352f46e32db5c
Andy Simmons <rocknswimmer116@gmail.com>
2022-10-19 11:19:59 -0400

    on change for question for fields with console log on button click confirmation

--- client/src/components/Questions/QuestionModal.jsx
+++ client/src/components/Questions/QuestionModal.jsx
@@ -1,21 +1,42 @@
 import React, { useState, useEffect } from 'react';
 
 const QuestionModal = (props) => {
+  const [question, setQuestion] = useState('');
+  const [questionUser, setQuestionUser] = useState('');
+  const [questionEmail, setQuestionEmail] = useState('');
+
+  const onQ = (e) => {
+    setQuestion(e.target.value);
+  };
+
+  const onQUser = (e) => {
+    setQuestionUser(e.target.value);
+  };
+
+  const onQEmail = (e) => {
+    setQuestionEmail(e.target.value);
+  };
+
+  const onSumbitQ = () => {
+    console.log(`submiting question: ${question} email: ${questionEmail} user: ${questionUser}`);
+  };
+
+
   return (
     <div className="modal">
       <div className="modal-pop">
         <button onClick={props.close}>x</button>
         <br />
         <lable for="question">Your Question*</lable>
-        <input type="text" name="question" />
+        <input type="text" name="question" onChange={onQ} />
         <br />
         <lable for="username">What is your nickname*</lable>
-        <input type="text" placeholder="Example: jackson11!" name="username" />
+        <input type="text" placeholder="Example: jackson11!" name="username" onChange={onQUser}/>
         <br />
         <lable for="email">Your email*</lable>
-        <input type="text" placeholder="Why did you like the product or not?" name="email" />
+        <input type="text" placeholder="Why did you like the product or not?" name="email" onChange={onQEmail}/>
         <br />
-        <button>Submit Question</button>
+        <button onClick={onSumbitQ}>Submit Question</button>
       </div>
       <div className="modal-overlay"></div>
     </div>
