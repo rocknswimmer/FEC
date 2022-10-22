a640014cc6e27f0b6c6f77ddafcc6a9640f29d31
Andy Simmons <rocknswimmer116@gmail.com>
2022-10-22 00:10:04 -0400

    thought of a way to lock load answer link below feed

--- client/src/components/Questions/AnswerFeed.jsx
+++ client/src/components/Questions/AnswerFeed.jsx
@@ -17,20 +17,21 @@ const AnswerFeed = (props) => {
     <div >
       <div className='answer-tag'>
         <strong>A: </strong>
-        {loadMoreAnswers && <a className='acollapse' onClick={loadAnswers}>Collapse answers</a>}
       </div>
       <div className='answer-feed'>
         {props.answers.length > 2 && !loadMoreAnswers && firstAnswers.map((answer, i) => {
           return <Answers key={i} answer={answer} />;
         })}
-        {props.answers.length > 2 && !loadMoreAnswers && <div className='answer-loader'><a onClick={loadAnswers}>Load More Answers</a></div>}
         {props.answers.length > 2 && loadMoreAnswers && props.answers.map((answer, i) => {
           return <Answers key={i} answer={answer} />;
         })}
-        {props.answers.length > 2 && loadMoreAnswers && <div className='answer-loader'><a className='acollapse' onClick={loadAnswers}>Collapse answers</a></div>}
         {props.answers.length <= 2 && props.answers.map((answer, i) => {
           return <Answers key={i} answer={answer} />;
         })}
+      </div>
+      <div>
+        {props.answers.length > 2 && !loadMoreAnswers && <div className='answer-loader'><a onClick={loadAnswers}>Load More Answers</a></div>}
+        {props.answers.length > 2 && loadMoreAnswers && <div className='answer-loader'><a className='acollapse' onClick={loadAnswers}>Collapse answers</a></div>}
       </div>
     </div>
   );
