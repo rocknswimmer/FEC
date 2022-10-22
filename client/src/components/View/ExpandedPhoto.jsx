c6bdf6becaf813f54f577492386508c9d95b93c5
Corinna Cole <corinna.j.cole@gmail.com>
2022-10-22 17:34:00 -0400

    work on expanded view

--- client/src/components/View/ExpandedPhoto.jsx
+++ client/src/components/View/ExpandedPhoto.jsx
@@ -1,29 +1,53 @@
 import React, { useState, useEffect } from 'react';
 import Carousel from './Carousel.jsx';
-import { FaAngleLeft, FaAngleRight, FaWindowClose } from 'react-icons/fa';
-import { ExpandedImageDiv, ExpandedView, ComponentBlock, LeftArrow, RightArrow, ModalPop, ModalPhoto, ModalOverlay } from './Styled/LargeImage.styled.jsx';
+import { FaCamera, FaAngleLeft, FaAngleRight, FaWindowClose, FaCircle, FaStar } from 'react-icons/fa';
+import { ExpandedImageDiv, ExpandedView, ComponentBlock, LeftArrow, RightArrow, ModalPop, ModalPhoto, ModalOverlay, IconHolder, ExpandedPhotoControls } from './Styled/LargeImage.styled.jsx';
 
 
-const ExpandedPhoto = ({ displayedImage, photoIndex, arrowRightHandler, expandPhoto, arrowLeftHandler}) => {
+const ExpandedPhoto = ({ displayedImage, photoIndex, arrowRightHandler, expandPhoto, arrowLeftHandler, lengthOfImageArray, changePhotoToSelectedIcon, imageArray }) => {
+
+
   const [isZoomed, setIsZoomed] = useState(false);
   const [mouseLocation, setMouseLocation] = useState('');
   // zoom clickhandler
+
+
   const zoom = (event) => {
     // console.log(event);
     setMouseLocation(`${event.clientX}px ${event.clientY}px`);
     setIsZoomed(!isZoomed);
   };
 
+
+
+
   return (
     <>
       <ExpandedImageDiv >
         <ModalPop className="modal-pop" role="dialog" aria-modal="true" >
-          <FaAngleLeft className="expanded-photo-left" onClick={arrowLeftHandler} />
 
-          <FaWindowClose onClick={expandPhoto} className="close-out-expanded-view"/>
+          <ExpandedPhotoControls>
+            <FaAngleLeft className="expanded-photo-left" onClick={arrowLeftHandler} />
+            <FaAngleRight className="expanded-photo-right" onClick={arrowRightHandler} />
+            <FaWindowClose onClick={expandPhoto} className="close-out-expanded-view" />
+
+            <IconHolder>
+              {
+                Array.from(Array(imageArray.length).keys()).map((num, index) => {
+                  if (num === photoIndex) {
+                    return <div key={index}><FaCamera className='icon-styled' /></div>;
+                  } else {
+                    return <div key={index} number={index} onClick={(e) => { changePhotoToSelectedIcon(index); }}><FaCircle style={{height: '20px'}} className='icon-styled' /></div>;
+
+                  }
+                })
+              }
+            </IconHolder>
+          </ExpandedPhotoControls>
+
           <ModalPhoto src={displayedImage} mouseLocation={mouseLocation} isZoomed={isZoomed} onClick={zoom} />
 
-          <FaAngleRight className="expanded-photo-right" onClick={arrowRightHandler} />
+
         </ModalPop>
         <ModalOverlay></ModalOverlay>
       </ExpandedImageDiv>
--- client/src/components/View/Styled/LargeImage.styled.jsx
+++ client/src/components/View/Styled/LargeImage.styled.jsx
@@ -14,16 +14,20 @@ const MainImage = styled.div`
   grid-template-rows: repeat(10, minmax(0, 1fr));
   margin-left: 20px;
   margin-right: 20px;
-  width: 100%;
   height: 100%;
   background-image: url(${props => props.img});
   background-position: center;
   background-size:cover;
     width: auto;
     height: atuo;
-    color: red;
+
     overflow: hidden;
   object-fit: cover;
+  svg {
+    stroke: #333;
+    stroke-width: 20px;
+    fill: #C5D7D9;
+  }
 `;
 
 const ExpandedView = styled.div`
@@ -32,6 +36,7 @@ const ExpandedView = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-template-rows: repeat(3, 100px);
+
 `;
 const PhotoColumn = styled.div`
   cursor: auto;
@@ -80,14 +85,12 @@ const ExpandedImageDiv = styled.div`
 const ModalPop = styled.div`
 display: flex;
 align-items: center;
-background: #fff;
+object-fit: fill;
+overflow: hidden;
+background: black;
 border: 2px solid #aaa;
-object-fit: contain;
-border-radius: 5px;
 z-index: 2;
 margin: auto;
-overflow-y: scroll;
-overflow: scroll;
 position: fixed;
 top: 50%;
 left: 50%;
@@ -95,18 +98,18 @@ max-width: 95vh;
 max-height: 95vw;
 transform: translate(-50%, -50%);
 svg {
-  color: purple;
+  stroke: #333;
+  stroke-width: 20px;
+  fill: #C5D7D9;
+  cursor: pointer;
 }
 `;
 
 const ModalPhoto = styled.img`
   transform: ${props => props.isZoomed ? 'scale(2.5, 2.5)' : null};
   transform-origin: ${props => props.mouseLocation};
-  background-position: center;
-  background-size:cover;
   max-height: 100vh;
   max-width: 100vw;
-  object-fit: cover;
 `;
 
 const ModalOverlay = styled.div`
@@ -120,7 +123,28 @@ background-color: rgb(0, 0, 0);
 opacity: 0.75;
 `;
 
-export { MainImage, ExpandedView, ComponentBlock, LeftArrow, RightArrow, Expander, PhotoColumn, Thumbnails, ExpandedImageDiv, ModalPop, ModalPhoto, ModalOverlay };
+const ExpandedPhotoControls = styled.div`
+width: 100%;
+display: flex;
+font-weight: 1em;
+flex-direction: row;
+position: fixed;
+justify-content: space-between;
+z-index: 4;
+`;
+
+const IconHolder = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  z-index: 4;
+  position: fixed;
+  top: 90%;
+
+`;
+
+export { MainImage, ExpandedView, ComponentBlock, LeftArrow, RightArrow, Expander, PhotoColumn, Thumbnails, ExpandedImageDiv, ModalPop, ModalPhoto, ModalOverlay, IconHolder, ExpandedPhotoControls };
 
 
 // background-image: url(${props => props.img});
