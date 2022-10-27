30d191ec30ace9735013f5d9de612279daf4dc1f
Corinna Cole <corinna.j.cole@gmail.com>
2022-10-27 10:19:29 -0400

    fix scroll lock

--- client/src/components/View/ProductImage.jsx
+++ client/src/components/View/ProductImage.jsx
@@ -83,10 +83,11 @@ const ProductImage = ({ currentDisplayedStyle, interact }) => {
     let scrollSetting = isScrollable;
     setIsScorllable(!scrollSetting);
     if (isScrollable) {
-      document.body.style.overflow = 'scroll';
+      document.body.style.overflow = 'hidden';
+
     }
     if (!isScrollable) {
-      document.body.style.overflow = 'hidden';
+      document.body.style.overflow = 'scroll';
     }
   };
 
